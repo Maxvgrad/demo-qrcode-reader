8ea323d6337d41934a0b4eeebbd5876d68b79c60
Maksim Vinogradov <maxvgrad@gmail.com>
2019-03-12 21:53:15 +0300

    Add QrCodeService

---
+++ src/main/java/ru/demo/reader/qrcode/service/QrCodeService.java
@@ -0,0 +1,37 @@
+package ru.demo.reader.qrcode.service;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+@Slf4j
+@Service
+public class QrCodeService {
+
+    public String read(MultipartFile qrCode) throws Exception {
+
+        BufferedImage bufferedImage = ImageIO.read(qrCode.getInputStream());
+
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            log.debug("There is no QR code in the image");
+            throw new IllegalArgumentException("There is no QR code in the image");
+        }
+
+    }
+}
